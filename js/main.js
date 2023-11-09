98e9673b9ea6c9ac8a16d578e52e81359258d118
Raymond Debasa Peralta <UO290054@uniovi.es>
2023-11-09 18:33:18 +0100

    Control de acceso

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
 const formulario = document.getElementById('miFormulario');
 const tablaDatos = document.getElementById('tablaDatos');
 
+// Reemplaza 'TU_USUARIO' y 'TU_TOKEN_DE_ACCESO' con tu información real
+const usuarioGitHub = 'UO290054';
+const accessToken = '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';
+
 // Cargar datos existentes al cargar la página
-fetch('data/datos.json')
+fetch(`https://api.github.com/repos/${usuarioGitHub}/${usuarioGitHub}.github.io/contents/data/datos.json`)
     .then(response => response.json())
     .then(data => {
-        data.forEach(usuario => agregarUsuarioATabla(usuario));
+        const datos = JSON.parse(atob(data.content));
+        datos.forEach(usuario => agregarUsuarioATabla(usuario));
     });
 
 formulario.addEventListener('submit', function (e) {
     e.preventDefault();
     const apodo = document.getElementById('apodo').value;
     const nombre = document.getElementById('nombre').value;
-    
+
     // Crear un objeto con los datos del usuario
     const nuevoUsuario = { apodo, nombre };
 
     // Agregar el nuevo usuario a la tabla
     agregarUsuarioATabla(nuevoUsuario);
 
-    // Guardar los datos en el archivo JSON
+    // Guardar los datos en el archivo JSON en GitHub
     guardarEnGitHub(nuevoUsuario);
-
+    
     // Limpiar los campos del formulario
     document.getElementById('apodo').value = '';
     document.getElementById('nombre').value = '';
@@ -37,9 +42,7 @@ function agregarUsuarioATabla(usuario) {
 }
 
 function guardarEnGitHub(usuario) {
-    const accessToken = 'TU_TOKEN_DE_ACCESO';
-
-    fetch('https://api.github.com/repos/tuusuario/tuusuario.github.io/contents/data/datos.json', {
+    fetch(`https://api.github.com/repos/${usuarioGitHub}/${usuarioGitHub}.github.io/contents/data/datos.json`, {
         method: 'GET',
         headers: {
             'Authorization': `token ${accessToken}`,
@@ -50,7 +53,7 @@ function guardarEnGitHub(usuario) {
         const datos = JSON.parse(atob(data.content));
         datos.push(usuario);
         
-        fetch('https://api.github.com/repos/tuusuario/tuusuario.github.io/contents/data/datos.json', {
+        fetch(`https://api.github.com/repos/${usuarioGitHub}/${usuarioGitHub}.github.io/contents/data/datos.json`, {
             method: 'PUT',
             headers: {
                 'Authorization': `token ${accessToken}`,
